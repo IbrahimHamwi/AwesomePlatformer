4171b97283d436c2a76b3c5b2b6556a680ed3726
IbrahimHamwi <ibrahimhamwi_619@hotmail.com>
2021-12-08 22:28:40 +0300

    Working on spider

--- Assets/Scripts/Enemy Scripts/SpiderScript.cs
+++ Assets/Scripts/Enemy Scripts/SpiderScript.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class SpiderScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator anim;
+    private Rigidbody2D myBody;
+
+    private Vector3 moveDirection = Vector3.down;
+
+    private string coroutine_Name = "ChangeMovement";
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        myBody = GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
         
@@ -13,6 +24,26 @@ public class SpiderScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MoveSpider();
+    }
+
+    void MoveSpider()
+    {
+        transform.Translate(moveDirection * Time.smoothDeltaTime);
+    }
+
+    IEnumerator ChangeMovement()
+    {
+        yield return new WaitForSeconds(Random.Range(2f, 5f));
+
+        if (moveDirection == Vector3.down)
+        {
+            moveDirection = Vector3.up;
+        }
+        else
+        {
+            moveDirection = Vector3.down;
+        }
+        StartCoroutine(ChangeMovement());
     }
 }
